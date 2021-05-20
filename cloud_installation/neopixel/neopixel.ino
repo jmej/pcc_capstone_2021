cc3f3a027ff8377c3ce86038a807a333e81e46b3
Dillon Brock <dillonbrock@dillons-mbp.lan>
2021-05-19 22:29:32 -0500

    three colors moving along strip with simplified logic, needs to be generalized to more than three colors

--- cloud_installation/neopixel/neopixel.ino
+++ cloud_installation/neopixel/neopixel.ino
@@ -29,49 +29,42 @@ void incomplete(int stripSpeed, int stripLength) {
     
     uint32_t firstPixelHue = 0;
     uint32_t lastTime = 0;
-    
-    int redTails[] = {0, 60, 120, 180};
-    int redHeads[] = {19, 79, 139, 199};
-    int blueTails[] = {20, 80, 140, 200};
-    int blueHeads[] = {39, 99, 159, 219};
-    int greenTails[] = {40, 100, 160, 220};
-    int greenHeads[] = {59, 119, 179, 239};
+
+    int tails[] = {0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220};
+    int heads[] = {19, 39, 59, 79, 99, 119, 139, 159, 179, 199, 219, 239};
 
     for(;;) {
-      for(int i = 0; i < strip.numPixels(); i++) {
-        for(int j = 0; j < 4; j++) {
-          if(((i >= redTails[j]) && (i <= redHeads[j])) || ((redTails[j] > redHeads[j]) && ((i >= redTails[j]) || (i <= redHeads[j])))) {
-            strip.setPixelColor(i, strip.Color(255, 0, 0));  
-          }
-          else if(((i >= blueTails[j]) && (i <= blueHeads[j])) || ((blueTails[j] > blueHeads[j]) && ((i >= blueTails[j]) || (i <= blueHeads[j])))) {
-            strip.setPixelColor(i, strip.Color(0, 0, 255));  
-          }
-          else if(((i >= greenTails[j]) && (i <= greenHeads[j])) || ((greenTails[j] > greenHeads[j]) && ((i >= greenTails[j]) || (i <= greenHeads[j])))) {
-            strip.setPixelColor(i, strip.Color(0, 255, 0));  
+      for (int i = 0; i < strip.numPixels(); i++) {
+        for (int j = 0; j < 12; j++) {
+          if (((i >= tails[j]) && (i <= heads[j])) || ((tails[j] > heads[j]) && ((i >= tails[j]) || (i <= heads[j])))) {
+            if (j == 0 || j == 3 || j == 6 || j == 9) {
+              strip.setPixelColor(i, strip.Color(255, 0, 0));  
+            }
+            else if (j == 1 || j == 4 || j == 7 || j == 10) {
+              strip.setPixelColor(i, strip.Color(0, 255, 0));  
+            }
+            else if (j == 2 || j == 5 || j == 8 || j == 11) {
+              strip.setPixelColor(i, strip.Color(0, 0, 255));  
+            }  
           }  
         }  
       }
 
       strip.show();
-
-      if((millis() - lastTime) > stripSpeed) {
-        for (int i = 0; i < 4; i++) {
-          if(++redHeads[i] >= strip.numPixels()) {
-            redHeads[i] = 0;
-            //if(++loopNum >= loops) return;  
-          }
-          if(++blueHeads[i] >= strip.numPixels()) {
-            blueHeads[i] = 0;  
-            //if(++loopNum >= loops) return;
-          }
-          if(++greenHeads[i] >= strip.numPixels()) {
-            greenHeads[i] = 0;
-            //if(++loopNum >= loops) return;  
-          }
-        }
-        lastTime = millis();   
-      }
-       
       
-  }
+      for (int i = 0; i < 12; i++) {
+        tails[i]++;
+        heads[i]++;
+        if (heads[i] >= strip.numPixels()) {
+          heads[i] = 0;  
+        }
+        if (tails[i] >= strip.numPixels()) {
+          tails[i] = 0;  
+        }  
+      }
+
+      delay(75);
+      
+    }   
+      
 }
