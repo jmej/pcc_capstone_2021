e4e93eda45ede1dc710a9815ff548a63be6055e7
Dillon Brock <dillonbrock@dillons-mbp.lan>
2021-05-19 23:39:52 -0500

    calls pulseWhite() function at random intervals

--- cloud_installation/neopixel/neopixel.ino
+++ cloud_installation/neopixel/neopixel.ino
@@ -7,10 +7,14 @@
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 
+int randBehaviorTime = random(10000, 20000);
+int lastTime = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
+  
   strip.begin();
   strip.show();
   strip.setBrightness(BRIGHTNESS);
@@ -18,7 +22,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  incompleteAmbience(75, 20);
+    incompleteAmbience(75, 20);
+    pulseWhite(5);
   
 }
 
@@ -26,6 +31,8 @@ void incompleteAmbience(int stripDelay, int stripLength) {
     if(stripLength >= strip.numPixels()) stripLength = strip.numPixels() -1;
     
     uint32_t lastTime = 0;
+
+    int loopNum = 0;
 
     int tails[] = {0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220};
     int heads[] = {19, 39, 59, 79, 99, 119, 139, 159, 179, 199, 219, 239};
@@ -53,7 +60,10 @@ void incompleteAmbience(int stripDelay, int stripLength) {
         tails[i]++;
         heads[i]++;
         if (heads[i] >= strip.numPixels()) {
-          heads[i] = 0;  
+          heads[i] = 0;
+          if (i == 0) {
+            loopNum++;
+          }  
         }
         if (tails[i] >= strip.numPixels()) {
           tails[i] = 0;  
@@ -61,7 +71,27 @@ void incompleteAmbience(int stripDelay, int stripLength) {
       }
 
       delay(stripDelay);
-      
-    }   
-      
+      //if (loopNum == 2) return;
+      if (millis() > randBehaviorTime) {
+        randBehaviorTime = millis() + random(10000, 20000);
+        return;
+      }
+
+    }
+
+}
+
+void pulseWhite(uint8_t wait) {
+  for(int j=0; j<256; j++) { // Ramp up from 0 to 255
+    // Fill entire strip with white at gamma-corrected brightness level 'j':
+    strip.fill(strip.Color(0, 0, 0, strip.gamma8(j)));
+    strip.show();
+    delay(wait);
+  }
+
+  for(int j=255; j>=0; j--) { // Ramp down from 255 to 0
+    strip.fill(strip.Color(0, 0, 0, strip.gamma8(j)));
+    strip.show();
+    delay(wait);
+  }
 }
